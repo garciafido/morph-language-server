492ed27fafc30895d479d4fc4232655ee8842340
Fido García <garciafido@gmail.com>
2020-04-07 09:28:55 -0300

    using wasm

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,35 +1,30 @@
 import * as path from 'path';
-// import * as TreeSitterParser from 'web-tree-sitter'
-import * as TreeSitterParser from 'tree-sitter';
-const morph = require('tree-sitter-morph');
+import * as TreeSitterParser from 'web-tree-sitter'
 
 
 class MorphParser {
 	parser: TreeSitterParser | undefined;
 	error: string = "None errors";
 	constructor() {
-		this.parser = new TreeSitterParser();
-		this.parser.setLanguage(morph);
-
-		// const wasmFilePath = path.join(__dirname, '../parser', '/tree-sitter-morph.wasm');
-		// const self = this;
-		// TreeSitterParser.init().then(
-		// 	() => {
-		// 		TreeSitterParser.Language.load(wasmFilePath).then(
-		// 			(morph) => {
-		// 				self.parser = new TreeSitterParser;
-		// 				self.parser.setLanguage(morph);
-		// 			},
-		// 			(reason) => {
-		// 				console.log(reason);
-		// 				self.error = reason;
-		// 			});
-		// 	},
-		// 	(reason) => {
-		// 		console.log(reason);
-		// 		self.error = reason;
-		// 	}
-		// );
+		const wasmFilePath = path.join(__dirname, '../parser', '/tree-sitter-morph.wasm');
+		const self = this;
+		TreeSitterParser.init().then(
+			() => {
+				TreeSitterParser.Language.load(wasmFilePath).then(
+					(morph) => {
+						self.parser = new TreeSitterParser;
+						self.parser.setLanguage(morph);
+					},
+					(reason) => {
+						console.log(reason);
+						self.error = reason;
+					});
+			},
+			(reason) => {
+				console.log(reason);
+				self.error = reason;
+			}
+		);
 	}
 
 	parse(sourceCode: string) {
